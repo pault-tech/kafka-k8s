cabb1fda7e1fa91057b373f9bcfe84420d95ae66
Paul T <psvmargs@gmail.com>
2023-05-03 20:21:14 +0000

    fix up

--- kompose.sh
+++ kompose.sh
@@ -4,3 +4,6 @@ curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-
 
 chmod +x ./kompose
 ./kompose convert -f docker-compose.y*ml
+
+# https://blog.tilt.dev/2019/09/16/tips-on-moving-your-dev-env-from-docker-compose-to-kubernetes.html
+# https://github.com/tilt-dev/tilt-example-docker-compose/blob/kompose-example/Tiltfile
