c9583c00275769c919ed8c8980d0ee8a3f0b9a7e
Paul T <psvmargs@gmail.com>
2023-05-03 20:46:25 +0000

    WIP

--- kompose.sh
+++ kompose.sh
@@ -7,3 +7,5 @@ chmod +x ./kompose
 
 # https://blog.tilt.dev/2019/09/16/tips-on-moving-your-dev-env-from-docker-compose-to-kubernetes.html
 # https://github.com/tilt-dev/tilt-example-docker-compose/blob/kompose-example/Tiltfile
+
+curl  https://raw.githubusercontent.com/tilt-dev/tilt-example-docker-compose/kompose-example/Tiltfile > Tiltfile
